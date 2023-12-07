87e173b8aab72435aeb8c9238e7c18e238c5cc1f
aajmorgan <ajmsd6@gmail.com>
2023-12-06 19:25:29 -0500

    settinggggggg

--- EagleVision/EagleVision/settings.py
+++ EagleVision/EagleVision/settings.py
@@ -85,7 +85,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 WSGI_APPLICATION = 'EagleVision.wsgi.application'
 
-API_ENDPOINT = "http://waitlistapi.railway.internal"
+API_ENDPOINT = "https://waitlistapi.railway.internal"
 
 # Celery Broker - Redis  
 CELERY_BROKER_URL = os.environ.get("REDIS_PRIVATE_URL")
